d22785e82574b9007fe7dac084fabe1692f35af4
jonasfabricio <167653009+jonasfabricio@users.noreply.github.com>
2024-09-16 17:47:37 -0300

    UpdatesComming

--- script.js
+++ script.js
@@ -1,17 +1,23 @@
 const mobileMenu = document.getElementById('menu-mobile');
-const toggleButton = document.getElementById('toggle');
 const sections = document.querySelectorAll('section');
-const nav = document.querySelector('nav');
 const allElements = document.querySelectorAll('*');
+const darkBall = document.getElementById('darkmode');
 let dark = true; 
 let show = false;
 
 function toggle(){
     if(dark == true){
-        document.body.style.backgroundColor='#d6d3d1';
-        nav.style.backgroundColor='#f5f5f4';
+        document.body.style.backgroundColor='#8f8381';
+        document.querySelector('nav').style.backgroundColor='#baa5a4';
+        document.getElementById('toggle').style.backgroundColor='#d6c3c1'
+        document.getElementById('toggle').style.boxShadow='0px 0px 6px 0px rgb(51, 51, 51)'
+        for(i=0;i<2;i++){
+            document.getElementsByClassName('project')[i].style.backgroundColor='#cbb0b0'
+        }
+        darkBall.style.backgroundColor='#ffffffff'
+        darkBall.style.left='18px'
         sections.forEach(section => {
-        section.style.backgroundColor='#f5f5f4';
+        section.style.backgroundColor='#baa5a4';
         })
         allElements.forEach(element => {
             element.style.color='black'
@@ -19,7 +25,14 @@ function toggle(){
         dark = false
     }else{
         document.body.style.backgroundColor='#404040';
-        nav.style.backgroundColor='#262626';
+        document.querySelector('nav').style.backgroundColor='#262626';
+        document.getElementById('toggle').style.backgroundColor='rgb(51, 51, 51)'
+        document.getElementById('toggle').style.boxShadow='0px 0px 6px 0px rgb(255, 255, 255)'
+        for(i=0;i<2;i++){
+            document.getElementsByClassName('project')[i].style.backgroundColor='#333333'
+        }
+        darkBall.style.backgroundColor='#ffffff4f'
+        darkBall.style.left='0px'
         sections.forEach(section => {
         section.style.backgroundColor='#262626';
         })
@@ -67,6 +80,12 @@ document.getElementById('skillsButtonMobile').addEventListener('click', function
 });
 
 
+
+document.getElementById('contateMe').addEventListener('click', function() {
+    document.getElementById('contato').scrollIntoView({
+        behavior: 'smooth'
+    });
+});
 
 document.getElementById('contatoButtonDesktop').addEventListener('click', function() {
     document.getElementById('contato').scrollIntoView({
